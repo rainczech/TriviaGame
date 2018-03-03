d21abb83b9ff3e7e7a353bb8dd2ea412d9a2bf6c
Grace Ho <rainczech@gmail.com>
2018-03-03 09:32:22 -0600

    js pseudocode and HTML framework

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+// press start to start game
+// question appears with countdown
+// if correct answer, show correct answer w/ photo/gif
+// if incorrect answer, show correct answer w/photo/gif
+// with no user input will go to next question
+// end wil show correct, incorrect, and unanswered array
+// start over does not reload page but resets the game
+
