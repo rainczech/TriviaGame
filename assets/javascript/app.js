e802532e07f9aa0abfa1ca2d5eecbf748a7c8c08
Grace Ho <rainczech@gmail.com>
2018-03-04 14:01:31 -0600

    some javascript added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,3 +6,65 @@
 // end wil show correct, incorrect, and unanswered array
 // start over does not reload page but resets the game
 
+// global variables
+
+// answers
+
+var correct = 0;
+var wrong = 0;
+var noResponse = 0;
+
+var question = [
+    new inquiry("Where did pizza originate from?", ["New York City", "Naples, Italy", "Paris, France", "Tokyo, Japan"], "Naples, Italy"),
+    new inquiry("The orignal name for 'processed' cheese is..?" ["embalmed cheese", "fake cheese", "cheese by-product", "pseudo-cheese"], "embalmed cheese")
+
+
+]
+
+
+
+
+
+
+
+// functions
+
+function inquiry(text, choice, answer){
+    this.text = text;
+    this.choice = choice;
+    this.answer = answer;
+
+
+};
+
+function correctAnswer(){
+    if(userChoice === this.answer){
+    correct++;}
+};
+
+function wrongAnswer(){
+    if(userChoice === this.answer){
+    wrong++;}
+}
+
+function noResponse (){
+    if(timeLeft === 0) {
+        noResponse++;
+    }
+}
+
+
+
+
+
+
+
+
+function restart(){
+    correct = 0;
+    wrong = 0;
+    noResponse = 0;
+}
+
+// main
+inquire ()
